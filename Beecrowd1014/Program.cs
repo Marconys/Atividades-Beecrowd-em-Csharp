8147160ad7d1ac7a0213e3e3124d1b46e2e43de5
Marconys <marconysbatera@gmail.com>
2022-08-14 13:01:09 -0300

    Resolvendo com o conceito de uma programação orientada a objetos

---
+++ Beecrowd1014/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd1014
+{  
+    class Consumo
+    {
+        public int Km;
+        public double Lt;
+
+        public Consumo()
+        {
+
+        }
+
+        public double CalcularConsumo()
+        {
+            return Km / Lt;
+        }
+
+        public override string ToString()
+        {
+            return CalcularConsumo().ToString("F3", CultureInfo.InvariantCulture) + " km/l"; 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Consumo consumo = new Consumo();
+
+            consumo.Km = int.Parse(Console.ReadLine());
+            consumo.Lt = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(consumo);
+
+            Console.ReadKey();
+        }
+    }
+}
