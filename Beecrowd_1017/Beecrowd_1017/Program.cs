2c3e4d3a19c99a89c48a67bfe64e5f0771938f82
Marconys <marconysbatera@gmail.com>
2023-01-25 14:20:23 -0300

    beecrowd 1017

---
+++ Beecrowd_1017/Beecrowd_1017/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Beecrowd_1017
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int horas = int.Parse(Console.ReadLine());
+            int velocidade = int.Parse(Console.ReadLine());
+
+            double distancia = (horas * velocidade);
+            double qtdLitros = (distancia / 12);
+
+            Console.WriteLine(qtdLitros.ToString("F3",CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
+        }
+    }
+}
