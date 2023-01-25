9056e156da213303c11fbc58082aead0cb63ed8a
Marconys <marconysbatera@gmail.com>
2023-01-24 21:39:43 -0300

    1016

---
+++ Beecrowd_1016/Distancia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distancia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Distancia = Int32.Parse(Console.ReadLine());
+
+            int tempo = Distancia * 2;
+
+            Console.WriteLine($"{tempo} minutos");
+            Console.ReadKey();
+        }
+    }
+}
