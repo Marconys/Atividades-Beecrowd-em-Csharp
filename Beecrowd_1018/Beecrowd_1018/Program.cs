93686cf54dab3df6a6bf871d8c3c3bd0f638d81d
Marconys <marconysbatera@gmail.com>
2023-02-07 12:43:41 -0300

    Usando Array para resolver a questão 1018 da beecrowd

---
+++ Beecrowd_1018/Beecrowd_1018/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beecrowd_1018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] notas = new int[7] { 100, 50, 20, 10, 5, 2, 1};
+
+            int valor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor);
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.WriteLine(valor / notas[i] + " nota(s) de R$ " + notas[i] + ",00");
+
+                valor = (valor%notas[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
