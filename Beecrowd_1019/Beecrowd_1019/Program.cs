997376dced668e88c69d97fb4e6f59fa937872f3
Marconys <marconysbatera@gmail.com>
2023-02-08 22:56:26 -0300

    beecrowd 1019

---
+++ Beecrowd_1019/Beecrowd_1019/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Beecrowd_1019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seg = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{seg /3600}:{(seg%3600)/60}:{seg%60}");
+
+            Console.ReadKey();
+        }
+    }
+}
