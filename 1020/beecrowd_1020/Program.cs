6517b28cb8410170b0ccc668d0d6242aac442116
Marconys <marconysbatera@gmail.com>
2023-02-27 21:29:50 -0300

    1020

---
+++ 1020/beecrowd_1020/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace beecrowd_1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32( Console.ReadLine());
+
+            int ano = x / 365;
+            x = (x%365);
+
+            int mes = x / 30;
+            x = (x%30);
+
+            int dias = x / 1;
+
+            Console.WriteLine($"{ano} ano(s)\n{mes} mes(es)\n{dias} dia(s)");
+        }
+    }
+}
